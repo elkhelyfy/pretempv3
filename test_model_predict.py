177019bdd01c54ca4bc528931c012c072c2aa54f
elkhelyfy <khelyfy.mohammed@gmail.com>
2024-07-05 19:02:08 +0100

    vercel fix

--- test_model_predict.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import joblib
-
-try:
-    with open('random_forest_tpot_model.pkl', 'rb') as model_file:
-        model = joblib.load(model_file)  # Adjust encoding if necessary
-    print("Model loaded successfully")
-except Exception as e:
-    print(f"Error loading model: {e}")
